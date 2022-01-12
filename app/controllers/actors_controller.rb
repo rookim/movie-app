e809d53c4c2edcd25c05d8759e081999862d3073
ro <kronew6@gmail.com>
2022-01-12 17:07:36 -0600

    installs bundle for ffaker gem and creates /one_actor route

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,7 @@
 class ActorsController < ApplicationController
+  def display_single_actor
+    id = params[:id]
+    actor = Actor.find_by(id: id)
+    render json: actor
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/one_actor" => "actors#display_single_actor"
 end
