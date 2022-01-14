683c9a5bd5b68a5c8d42da7e5d1c7a16a5e5a264
ro <kronew6@gmail.com>
2022-01-14 10:19:02 -0600

    finishes adding data to seeds.rb file and creates a movies controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,4 @@
 
 # actor = Actor.create({ first_name: first, last_name: last, known_for: known })
 
-movie = Movie.create(title: "A Silent Voice", year 2016, plot: "")
+movie = Movie.create(title: "A Silent Voice", year 2016, plot: "High school student Shoya has every intention of killing himself, but changes his mind at the very last minute. A flashback reveals Shoya in 6th grade relentlessly bullying a deaf girl named Shoko. When word of the bullying reaches the principal, Shoya is singled out by everyone else as the only culprit. THe bullying originally targeted at Shoko is now directed at Shoya. Shoya becomes an outcast throughout middle and high school for his reputation as a bully and as a result, he is a depressed loner unable to look at others in the eyes.")
