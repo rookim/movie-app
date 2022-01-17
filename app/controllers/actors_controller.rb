ab4b8482a2c56647365f0c9ffdb096d3fe7c25cf
ro <kronew6@gmail.com>
2022-01-17 17:53:38 -0600

    adds routes following RESTful conventions for Actor model

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,11 +4,34 @@ class ActorsController < ApplicationController
   end
 
   def create
-    
+    actor = Actor.create(
+      "first_name": params[:first_name],
+      "last_name": params[:last_name],
+      "known_for": params[:known_for]
+    )
+    render json: actor.as_json
   end
 
   def show
     id = params[:id]
     render json: Actor.find(id)
+  end
+
+  def update
+    id = params[:id]
+    actor = Actor.find(id)
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
+    actor.known_for = params[:known_for] || actor.known_for
+    actor.save
+
+    render json: actor.as_json
+  end
+
+  def destroy
+    actor = Actor.find(params[:id])
+    actor.destroy
+
+    render json: {"message": "Data ##{actor.id} has been destroyed....."}
   end
 end
