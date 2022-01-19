46d806364e1674550664c3773eaa5024de437d23
ro <kronew6@gmail.com>
2022-01-18 23:07:13 -0600

    modifies actors controller to render gender and age attributes

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,7 +7,9 @@ class ActorsController < ApplicationController
     actor = Actor.create(
       "first_name": params[:first_name],
       "last_name": params[:last_name],
-      "known_for": params[:known_for]
+      "known_for": params[:known_for],
+      "gender": params[:gender],
+      "age": params[:age]
     )
     render json: actor.as_json
   end
@@ -23,6 +25,8 @@ class ActorsController < ApplicationController
     actor.first_name = params[:first_name] || actor.first_name
     actor.last_name = params[:last_name] || actor.last_name
     actor.known_for = params[:known_for] || actor.known_for
+    actor.gender = params[:gender] || actor.gender
+    actor.age = params[:age] || actor.age
     actor.save
 
     render json: actor.as_json
