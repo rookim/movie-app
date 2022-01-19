7edf308eb6d3a7552bead988f77eef4200317f73
ro <kronew6@gmail.com>
2022-01-18 23:01:53 -0600

    edits movies controller to render director and english attributes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,9 @@ class MoviesController < ApplicationController
     movie = Movie.create(
       "title": params[:title],
       "year": params[:year],
-      "plot": params[:plot]
+      "plot": params[:plot],
+      "director": params[:director]
+      "english": params[:english]
     )
     render json: movie
   end
@@ -22,7 +24,9 @@ class MoviesController < ApplicationController
     movie = Movie.find(params[:id])
     movie.title = params[:title] || movie.title
     movie.year = params[:year] || movie.year
-    movie.plot = params[:plot] || movie.plot
+    movie.plot = params[:plot] || movie.plot 
+    movie.director = params[:director] || movie.director 
+    movie.english = params[:english] || movie.english 
     movie.save
 
     render json: movie
