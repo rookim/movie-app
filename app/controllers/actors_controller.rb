d863070c626b223d06ddddc711f73ca559c7aa3f
ro <kronew6@gmail.com>
2022-01-20 21:55:22 -0600

    edits create and update actions in actors controller to deal with happy/sad paths

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,14 +4,18 @@ class ActorsController < ApplicationController
   end
 
   def create
-    actor = Actor.create(
+    actor = Actor.new(
       "first_name": params[:first_name],
       "last_name": params[:last_name],
       "known_for": params[:known_for],
       "gender": params[:gender],
       "age": params[:age]
     )
-    render json: actor.as_json
+    if actor.save
+      render json: actor.as_json
+    else
+      render json: {errors: actor.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -20,16 +24,17 @@ class ActorsController < ApplicationController
   end
 
   def update
-    id = params[:id]
-    actor = Actor.find(id)
+    actor = Actor.find(params[:id])
     actor.first_name = params[:first_name] || actor.first_name
     actor.last_name = params[:last_name] || actor.last_name
     actor.known_for = params[:known_for] || actor.known_for
     actor.gender = params[:gender] || actor.gender
     actor.age = params[:age] || actor.age
-    actor.save
-
-    render json: actor.as_json
+    if actor.save
+      render json: actor.as_json
+    else
+      render json: {errors: actor.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
