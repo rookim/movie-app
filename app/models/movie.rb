e3cff54a9c43ff0b3b68701c19b0cc9089a0f6cb
ro <kronew6@gmail.com>
2022-01-20 22:32:12 -0600

    makes changes to length helper in Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   validates :title, :year, :plot, :director, :english, presence: true
   validates :year, numericality: { only_integer: true, greater_than: 1887 }
-  validates :plot, length: { maximum: 500, too_long: "only allows for a maximum of #{count} characters."}
+  validates :plot, length: {maximum: 1000, too_long: "only allows for a maximum of #{count} characters"}
 end
