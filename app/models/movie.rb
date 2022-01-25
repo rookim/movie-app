7a08d50328f22f0f816315cfe7e62e8977dcc099
ro <kronew6@gmail.com>
2022-01-24 18:03:03 -0600

    adds associations for Actor and Movie models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, :year, :plot, :director, :english, presence: true
   validates :year, numericality: { only_integer: true, greater_than: 1887 }
   validates :plot, length: {maximum: 1000, too_long: "only allows for a maximum of #{count} characters"}
+
+  has_many :actors
 end
