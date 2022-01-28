7eb942d98efde9ec86dbff0a579ec8c936502eab
ro <kronew6@gmail.com>
2022-01-28 09:50:10 -0600

    adds before_action at the top

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
+  
   def index
     english_movies = Movie.where(english: "true")
     render json: english_movies.as_json
