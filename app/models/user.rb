0b7271a8cc10d1486ef31a79ee40d4260ca87185
ro <kronew6@gmail.com>
2022-02-23 10:59:41 -0600

    adds validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, length: { in: 2..25 }
+  validates :password ,length: { in: 8..20 }
 end
